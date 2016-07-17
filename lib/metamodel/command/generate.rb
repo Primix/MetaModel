7d99d66b3df72a35e496522259dc0e35b3795fc1
Draveness <stark.draven@gmail.com>
2016-07-17 10:35:06 +0800

    Add desc to command generate

--- lib/metamodel/command/generate.rb
+++ lib/metamodel/command/generate.rb
@@ -3,6 +3,8 @@ module MetaModel
     class Generate < Command
       self.summary = "Generate a class skeleton for a model."
       self.description = <<-DESC
+        Generate a skeleton for a Objective-C/Swift class and create
+        this file as model.rb in MetaModel folder.
       DESC
 
       def initialize(argv)
