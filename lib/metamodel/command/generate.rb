46bfc6cd14ab9e807910b9e8476a0508a4e8ca49
Draveness <stark.draven@gmail.com>
2016-07-17 16:44:49 +0800

    Finish generate command skeleton

--- lib/metamodel/command/generate.rb
+++ lib/metamodel/command/generate.rb
@@ -1,3 +1,6 @@
+require 'pathname'
+require 'active_support/core_ext/string/strip'
+
 module MetaModel
   class Command
     class Generate < Command
@@ -8,11 +11,26 @@ module MetaModel
       DESC
 
       def initialize(argv)
+        @model_name = argv.shift_argument
+        @file_path = Pathname.pwd + "#{@model_name.downcase}.rb"
         super
       end
 
       def run
+        @file_path.open('w') { |f| f << model_template(@model_name) }
+      end
 
+      private
+
+      def model_template(model)
+        modelfile = ''
+        modelfile << "metamodel_version '#{VERSION}'\n\n"
+        modelfile << <<-MODEL.strip_heredoc
+          define :#{model} do |j|
+            # define #{model} model like this
+            # j.string 'nickname'
+          end
+        MODEL
       end
 
     end
