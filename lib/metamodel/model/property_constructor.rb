ed1048047b85e20080a162019a50f7a56ee610bf
Draveness <stark.draven@gmail.com>
2016-07-20 13:42:43 +0800

    Use method missing to generate model

--- lib/metamodel/model/property_constructor.rb
+++ lib/metamodel/model/property_constructor.rb
@@ -4,12 +4,19 @@ module MetaModel
 
     attr_reader :model
 
+    define_method :string do |json_key, property_key = nil|
+      save_property CocoaProperty.new(json_key, :string, property_key)
+    end
+
     def initialize(model)
       @model = model
     end
 
-    def string(json_key, property_key = nil)
-      save_property CocoaProperty.new(json_key, "String", property_key)
+    def method_missing(symbol, *arguments)
+      define_method symbol do |json_key, property_key = nil|
+        save_property CocoaProperty.new(json_key, symbol, property_key)
+      end
+      eval symbol, arguments
     end
 
     private
