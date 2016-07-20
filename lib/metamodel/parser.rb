1470b321e604c0c21be25b66e615d77807897740
Draveness <stark.draven@gmail.com>
2016-07-20 11:26:21 +0800

    Remove puts method in parser

--- lib/metamodel/parser.rb
+++ lib/metamodel/parser.rb
@@ -10,7 +10,6 @@ module MetaModel
       @scaffold_path = scaffold_path
       @models = []
       parse
-      puts @models
     end
 
     def parse
@@ -26,7 +25,6 @@ module MetaModel
     def metamodel_version(version)
       raise Informative,
         "Scaffold file #{version} not matched with current metamodel version #{VERSION}" if version != VERSION
-      puts version
     end
 
     def define(model_name)
