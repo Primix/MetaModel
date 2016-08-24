709802004d826e9847267cf54530e2d22146f199
Draveness <stark.draven@gmail.com>
2016-08-24 15:22:10 +0800

    Add mofifiers to model

--- lib/metamodel/model/cocoa_property.rb
+++ lib/metamodel/model/cocoa_property.rb
@@ -5,6 +5,7 @@ module MetaModel
     attr_reader :key
     attr_reader :type
     attr_reader :modifiers
+    attr_reader :default_value
 
     def initialize(json_key, type = :string, *modifiers)
       @json_key = json_key
@@ -17,6 +18,18 @@ module MetaModel
 
     def convert_symbol_to_type(symbol)
       symbol.to_s.capitalize
+    end
+
+    def is_unique?
+      @modifiers.include? :unique
+    end
+
+    def is_primary?
+      @modifiers.include? :primary
+    end
+
+    def has_default_value?
+      @modifiers[:default].nil?
     end
 
   end
--- scaffold/user.rb
+++ scaffold/user.rb
@@ -4,5 +4,5 @@ define :User do |j|
   # define User model like this
   j.nickname :string
   j.avatar :string
-  j.email :string, :unique
+  j.email :string, :unique, default: "default@gmail.com"
 end
