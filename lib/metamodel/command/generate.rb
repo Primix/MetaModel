be7bb23d5c2ab90374de10d0585e14c3eba5ecd6
Draveness <stark.draven@gmail.com>
2016-08-26 16:47:50 +0800

    Add debug infomation in generate command file

--- lib/metamodel/command/generate.rb
+++ lib/metamodel/command/generate.rb
@@ -16,7 +16,14 @@ module MetaModel
 
       def run
         verify_scaffold_exists!
-        @file_path.open('w') { |f| f << model_template(@model_name) }
+        UI.section "Generating model scaffold file" do
+          title_options = { :verbose_prefix => '-> '.green }
+          UI.titled_section "Adding `#{File.basename(@file_path)}` to scaffold folder", title_options do
+            @file_path.open('w') { |f| f << model_template(@model_name) }
+          end
+          UI.notice "`#{File.basename(@file_path)}` has already generated, use the command below to edit it.\n"
+          UI.message "vim scaffold/#{File.basename(@file_path)}"
+        end
       end
 
       private
