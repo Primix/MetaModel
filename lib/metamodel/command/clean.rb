f8289274bf377dc0b6712c5f191c74e53b47badb
Draveness <stark.draven@gmail.com>
2016-08-26 11:48:50 +0800

    Fixed fileutils name error

--- lib/metamodel/command/clean.rb
+++ lib/metamodel/command/clean.rb
@@ -1,7 +1,8 @@
+require 'fileutils'
+
 module MetaModel
   class Command
     class Clean < Command
-      require 'fileutil'
 
       self.summary = "Clean MetaModel project from current folder."
       self.description = <<-DESC
@@ -15,7 +16,7 @@ module MetaModel
 
       def run
         UI.section "Removing MetaModel project" do
-          FileUtil.rm_rf 'MetaModel'
+          FileUtils.rm_rf 'MetaModel'
           UI.message "Already clean up the whole MetaModel project from current folder"
         end
       end
