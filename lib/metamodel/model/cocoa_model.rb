135cb117a69b04c7b3207dd12d6f223b0c0f7686
Draveness <stark.draven@gmail.com>
2016-09-05 22:17:02 +0800

    Update template file to fix #2

--- lib/metamodel/model/cocoa_model.rb
+++ lib/metamodel/model/cocoa_model.rb
@@ -32,6 +32,10 @@ module MetaModel
       end
     end
 
+    def hash_value
+      self.hash.to_s
+    end
+
     def property_key_value_pairs
       @properties.map { |property| "#{property.key.to_s}: #{property.key.to_s}" }.join(", ")
     end
