77ceef81342ba2969c2a344880204d08e5ca2c8d
Draveness <stark.draven@gmail.com>
2016-09-07 12:19:46 +0800

    Bump version to 0.1.0 and release first usable gem

--- lib/metamodel/version.rb
+++ lib/metamodel/version.rb
@@ -1,5 +1,5 @@
 module MetaModel
   # The version of the MetaModel command line tool.
   #
-  VERSION = '0.0.5'.freeze unless defined? MetaModel::MetaModel
+  VERSION = '0.1.0'.freeze unless defined? MetaModel::MetaModel
 end
