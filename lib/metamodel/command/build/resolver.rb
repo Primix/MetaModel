22f05278019056c17ca106cc3f4f54795cfeba26
Draveness <stark.draven@gmail.com>
2016-09-10 16:12:55 +0800

    Enclose model and property class into Record module

--- lib/metamodel/command/build/resolver.rb
+++ lib/metamodel/command/build/resolver.rb
@@ -5,8 +5,8 @@ module MetaModel
 
         include Config::Mixin
 
-        require 'metamodel/model/model'
-        require 'metamodel/model/property'
+        require 'metamodel/record/model'
+        require 'metamodel/record/property'
         require 'metamodel/command/build/renderer'
 
         attr_accessor :models
@@ -34,30 +34,30 @@ module MetaModel
 
         def define(model_name)
           UI.message '-> '.green + "Resolving `#{model_name.to_s.camelize}`"
-          @models << Model.new(model_name)
+          @models << Record::Model.new(model_name)
           yield
         end
 
         def attr(key, type = :string, *args)
-          current_model.properties << Property.new(key, type, args)
+          current_model.properties << Record::Property.new(key, type, args)
         end
 
         def has_one(name, model = nil)
           model = name.to_s.singularize.camelize if model.nil?
-          current_model.relation_properties << Property.new(name, model, :has_one)
+          current_model.relation_properties << Record::Property.new(name, model, :has_one)
         end
 
         def has_many(name, model = nil)
           model = name.to_s.singularize.camelize if model.nil?
-          property = Property.new(name, model, :has_many)
+          property = Record::Property.new(name, model, :has_many)
           raise Informative, "Property type in has_many relation can't be optional" if property.is_optional?
           current_model.relation_properties << property
         end
 
         def belongs_to(name, model = nil)
           model = name.to_s.singularize.camelize if model.nil?
-          current_model.relation_properties << Property.new(name, model, :belongs_to)
-          current_model.properties << Property.new("#{name}_id".camelize, "Int", :foreign, :default => 0)
+          current_model.relation_properties << Record::Property.new(name, model, :belongs_to)
+          current_model.properties << Record::Property.new("#{name}_id".camelize, "Int", :foreign, :default => 0)
         end
 
         private
