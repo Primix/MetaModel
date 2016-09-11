d15cb259291d5af40da874b35881a66c689949d5
Draveness <stark.draven@gmail.com>
2016-09-12 00:34:21 +0800

    Bump version to 0.1.8

--- lib/metamodel/version.rb
+++ lib/metamodel/version.rb
@@ -1,5 +1,5 @@
 module MetaModel
   # The version of the MetaModel command line tool.
   #
-  VERSION = '0.1.7' unless defined? MetaModel::VERSION
+  VERSION = '0.1.8' unless defined? MetaModel::VERSION
 end
