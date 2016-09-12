0e117f2942d3503e2d67c9d14b00becb5744b71a
Draveness <stark.draven@gmail.com>
2016-09-12 19:33:15 +0800

    Add append and create method

--- lib/metamodel/template/model_initialize.swift
+++ lib/metamodel/template/model_initialize.swift
@@ -48,5 +48,10 @@ public extension <%= model.relation_name %> {
     func create(<%= model.property_key_type_pairs_without_property property.name %>) -> <%= model.name %>? {
         return <%= model.name %>.create(<% if model.properties_exclude_property(property).count == 0 %><%= "#{property}: property" %><% else %><%= model.property_key_value_pairs %><% end %>)
     }
+
+    func append(element: <%= model.name %>) {
+        var element = element
+        element.<%= property.name %> = <%= property.name %>
+    }
     <% end %>
 }
