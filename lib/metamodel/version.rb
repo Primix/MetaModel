17dcc1a812560961d6ab5e76467e6bc278cbf674
Draveness <stark.draven@gmail.com>
2016-09-13 12:07:14 +0800

    Bump version to 0.2.0 and remome through support

--- lib/metamodel/version.rb
+++ lib/metamodel/version.rb
@@ -1,5 +1,5 @@
 module MetaModel
   # The version of the MetaModel command line tool.
   #
-  VERSION = '0.1.9' unless defined? MetaModel::VERSION
+  VERSION = '0.2.0' unless defined? MetaModel::VERSION
 end
