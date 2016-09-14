3d30aa97b545474e9bc4533c37f0d4cc359d51ca
Draveness <stark.draven@gmail.com>
2016-09-14 23:15:03 +0800

    Refactor build_metamodel_framework methods to different methods

--- lib/metamodel/installer.rb
+++ lib/metamodel/installer.rb
@@ -54,43 +54,56 @@ module MetaModel
 
     def build_metamodel_framework
       UI.section "Generating MetaModel.framework" do
-
-        build_iphoneos = "xcodebuild -scheme MetaModel \
-          -project MetaModel/MetaModel.xcodeproj \
-          -configuration Release -sdk iphoneos \
-          -derivedDataPath './metamodel' \
-          BITCODE_GENERATION_MODE=bitcode \
-          ONLY_ACTIVE_ARCH=NO \
-          CODE_SIGNING_REQUIRED=NO \
-          CODE_SIGN_IDENTITY= \
-          clean build"
-        build_iphonesimulator = "xcodebuild -scheme MetaModel \
-          -project MetaModel/MetaModel.xcodeproj \
-          -configuration Release -sdk iphonesimulator \
-          -derivedDataPath './metamodel' \
-          ONLY_ACTIVE_ARCH=NO \
-          CODE_SIGNING_REQUIRED=NO \
-          CODE_SIGN_IDENTITY= \
-          clean build"
-        result = system "#{build_iphoneos} > /dev/null &&
-          #{build_iphonesimulator} > /dev/null"
-
-        raise Informative, 'Building framework failed.' unless result
-
-        build_products_folder = "./metamodel/Build/Products"
-
-        copy_command = "cp -rf #{build_products_folder}/Release-iphoneos/MetaModel.framework . && \
-          cp -rf #{build_products_folder}/Release-iphonesimulator/MetaModel.framework/Modules/MetaModel.swiftmodule/* \
-                  MetaModel.framework/Modules/MetaModel.swiftmodule/"
-        lipo_command = "lipo -create -output MetaModel.framework/MetaModel \
-          #{build_products_folder}/Release-iphonesimulator/MetaModel.framework/MetaModel \
-          #{build_products_folder}/Release-iphoneos/MetaModel.framework/MetaModel"
-
-        result = system "#{copy_command} && #{lipo_command}"
-
-        raise Informative, 'Copy framework to current folder failed.' unless result
-        UI.message "-> ".green + "MetaModel.framework located in current folder"
+        build_framework_on_iphoneos
+        build_framework_on_iphonesimulator
+        copy_framework_swiftmodule_files
+        lipo_frameworks_on_different_archs
       end
+      UI.message "-> ".green + "MetaModel.framework located in current folder"
+    end
+
+    def build_framework_on_iphoneos
+      build_iphoneos = "xcodebuild -scheme MetaModel \
+        -project MetaModel/MetaModel.xcodeproj \
+        -configuration Release -sdk iphoneos \
+        -derivedDataPath './metamodel' \
+        BITCODE_GENERATION_MODE=bitcode \
+        ONLY_ACTIVE_ARCH=NO \
+        CODE_SIGNING_REQUIRED=NO \
+        CODE_SIGN_IDENTITY= \
+        clean build"
+      result = system "#{build_iphoneos} > /dev/null"
+      raise Informative, 'Building framework on iphoneos failed.' unless result
+    end
+
+    def build_framework_on_iphonesimulator
+      build_iphonesimulator = "xcodebuild -scheme MetaModel \
+        -project MetaModel/MetaModel.xcodeproj \
+        -configuration Release -sdk iphonesimulator \
+        -derivedDataPath './metamodel' \
+        ONLY_ACTIVE_ARCH=NO \
+        CODE_SIGNING_REQUIRED=NO \
+        CODE_SIGN_IDENTITY= \
+        clean build"
+        result = system "#{build_iphonesimulator} > /dev/null"
+        raise Informative, 'Building framework on iphonesimulator failed.' unless result
+    end
+
+    BUILD_PRODUCTS_FOLDER = "./metamodel/Build/Products"
+
+    def copy_framework_swiftmodule_files
+      copy_command = "cp -rf #{BUILD_PRODUCTS_FOLDER}/Release-iphoneos/MetaModel.framework . && \
+        cp -rf #{BUILD_PRODUCTS_FOLDER}/Release-iphonesimulator/MetaModel.framework/Modules/MetaModel.swiftmodule/* \
+                MetaModel.framework/Modules/MetaModel.swiftmodule/"
+      system copy_command
+    end
+
+    def lipo_frameworks_on_different_archs
+      lipo_command = "lipo -create -output MetaModel.framework/MetaModel \
+        #{BUILD_PRODUCTS_FOLDER}/Release-iphonesimulator/MetaModel.framework/MetaModel \
+        #{BUILD_PRODUCTS_FOLDER}/Release-iphoneos/MetaModel.framework/MetaModel"
+      result = system "#{lipo_command}"
+      raise Informative, 'Copy framework to current folder failed.' unless result
     end
 
   end
