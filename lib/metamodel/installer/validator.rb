047cb5f0a0dbfd2485884a4801e5003da3b12af5
Draveness <stark.draven@gmail.com>
2016-09-16 23:16:09 +0800

    Fixed validator class name

--- lib/metamodel/installer/validator.rb
+++ lib/metamodel/installer/validator.rb
@@ -1,55 +1,50 @@
 module MetaModel
-  class Installer
-    class Validator
-      require 'metamodel/record/model'
-      require 'metamodel/record/property'
-      require 'metamodel/record/association'
+  class Command
+    class Install
+      class Validator
+        require 'metamodel/record/model'
+        require 'metamodel/record/property'
+        require 'metamodel/record/association'
 
-      def translate!
-        satisfy_constraint = @associations.reduce([]) do |remain, association|
-          expect = remain.select { |assoc| assoc.expect_constraint? association }
-          if expect.empty?
-            remain << association
-          else
-            remain.delete expect.first
-          end
-          remain
+        attr_reader :models
+        attr_reader :associations
+
+        def initialize(models, associations)
+          @models = models
+          @associations = associations
         end
-        raise Informative, "Unsatisfied constraints in #{satisfy_constraint.map \
-          { |x| x.debug_description }}" if satisfy_constraint.size > 0
 
+        def translate
+          name_model_hash = Hash[@models.collect { |model| [model.name, model] }]
+          @associations.map! do |association|
+            major_model = name_model_hash[association.major_model]
+            major_model.associations << association
+            association.major_model = major_model
+            association.secondary_model = name_model_hash[association.secondary_model]
+            raise Informative, "Associations not satisfied in `Metafile`" \
+              unless [association.major_model, association.secondary_model].compact.size == 2
+            association
+          end
+
+          satisfy_constraint = @associations.reduce([]) do |remain, association|
+            expect = remain.select { |assoc| assoc.expect_constraint? association }
+            if expect.empty?
+              remain << association
+            else
+              remain.delete expect.first
+            end
+            remain
+          end
+          raise Informative, "Unsatisfied constraints in #{satisfy_constraint.map \
+            { |x| x.debug_description }}" \
+            if satisfy_constraint.size > 0
+
+          @models.each do |model|
+            model.properties.uniq! { |prop| [prop.name] }
+          end
+          return @models, @associations
+        end
       end
-
-      def validate_models
-        existing_types = @models.map { |m| m.properties.map { |property| property.type } }.flatten.uniq
-        unsupported_types = existing_types - supported_types
-        raise Informative, "Unsupported types #{unsupported_types}" unless unsupported_types == []
-      end
-
-      CURRENT_SUPPORTED_BUILT_IN_TYPES = %w[
-        Int
-        Double
-        Float
-        String
-        Bool
-        NSDate
-      ]
-
-      def built_in_types
-        CURRENT_SUPPORTED_BUILT_IN_TYPES.map do |t|
-          [t, "#{t}?"]
-        end.flatten
-      end
-
-      def supported_types
-        @models.reduce(CURRENT_SUPPORTED_BUILT_IN_TYPES) { |types, model|
-          types << model.name.to_s
-        }.map { |type|
-          [type, "#{type}?"]
-        }.flatten
-      end
-
-
     end
   end
 end
